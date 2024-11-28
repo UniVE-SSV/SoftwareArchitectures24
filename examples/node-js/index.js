2c3d515e47d536565e3ad789bccb8adeebffdb14
Giacomo Zanatta <giacomo.zanatta@unive.it>
2024-11-28 14:21:32 +0100

    Add examples

--- examples/node-js/index.js
+++ examples/node-js/index.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const axios = require('axios');  // Import Axios for HTTP requests
+const cors = require('cors');
 
 const app = express();
-
+app.use(cors());
 // Define the /student route
 app.get('/students', async (req, res) => {
-  res.status(200).json({id: 123, name: "Giacomo", surname: "Zanatta"});
+  const response = await axios.get('http://spring-app:8888/students');
+  console.log(response.data)
+  res.status(200).json(response.data);
 });
 
 // Set up the server to listen on a specific port and hostname
-const hostname = '127.0.0.1';
+const hostname = '0.0.0.0';
 const port = 3000;
 
 app.listen(port, hostname, () => {
